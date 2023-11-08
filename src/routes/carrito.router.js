d3e9740bed901f2568d525095059ff41eca0f40b
gianfranco cherubini <cherussa@gmail.com>
2023-11-08 19:42:49 -0300

    agregando ruta raiz post

--- src/routes/carrito.router.js
+++ src/routes/carrito.router.js
@@ -19,3 +19,36 @@ async function saveCart(cart) {
 
 const router = Router();
 
+router.post('/', async (req, res) => {
+    try {
+        const { id } = req.body;
+        if (isNaN(id)) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400).json({ error: 'El ID del carrito debe ser un número' });
+            return;
+        }
+
+        const carts = await getCart();
+        const existingCart = carts.find(cart => cart.id === id);
+
+        if (existingCart) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400).json({ error: 'El carrito con el mismo ID ya existe' });
+            return;
+        }
+
+        const newCart = {
+            id,
+            products: []
+        };
+
+        carts.push(newCart);
+        await saveCart(carts);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(201).json(newCart);
+    } catch (error) {
+        console.error("Error al crear el carrito: ", error);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).json({ error: 'Error al crear el carrito' });
+    }
+});
